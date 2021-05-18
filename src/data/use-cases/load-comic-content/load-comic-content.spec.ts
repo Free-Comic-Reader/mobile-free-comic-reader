ba0870f38f302106a4bc9eb83227080ced66d018
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-05-18 17:23:19 -0300

    test: create load comic content use case tests

---
+++ src/data/use-cases/load-comic-content/load-comic-content.spec.ts
@@ -0,0 +1,26 @@
+import FileManagerSpy from '../../test/file-manager-spy';
+import LoadComicContentUseCaseLocal from './load-comic-content-local';
+
+describe('Load Comic Content Use Case Test', () => {
+  it('Should return files load successful', async () => {
+    const fileManagerSpy = new FileManagerSpy();
+    fileManagerSpy.files = ['file://temp/file.jpg'];
+
+    const useCase = new LoadComicContentUseCaseLocal(fileManagerSpy);
+    const files = await useCase.run({filePath: 'temp/comic.cbr'});
+
+    expect(files).toEqual(['file://temp/file.jpg']);
+    expect(fileManagerSpy.filePath).toEqual('temp/comic.cbr');
+  });
+
+  it('Throw error if load fail', async () => {
+    const fileManagerSpy = new FileManagerSpy();
+    fileManagerSpy.error = new Error('Error simulation');
+
+    const useCase = new LoadComicContentUseCaseLocal(fileManagerSpy);
+
+    await expect(useCase.run({filePath: 'temp/comic.cbr'})).rejects.toEqual(
+      Error('Error simulation'),
+    );
+  });
+});
