c75878c8817494048e0ce1e784da8ff8753506bc
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-05-29 18:16:20 -0300

    test: delete comic repository test

--- src/infra/repository/create-comic-repository/create-comic-repository.spec.ts
+++ src/infra/repository/create-comic-repository/create-comic-repository.spec.ts
@@ -1,3 +1,40 @@
+import ComicDatabaseSpy from '../../test/comic-database-spy';
+import CreateComicRepositoryLocal from './create-comic-repository-local';
+
 describe('Create comic repository', () => {
-  it('', () => {});
+  it('Should return data if create successful', async () => {
+    const databaseSpy = new ComicDatabaseSpy();
+    databaseSpy.result = {
+      name: 'Dororo',
+      filePath: 'temp/dororo',
+      lastPageSeen: 1,
+    };
+
+    const repository = new CreateComicRepositoryLocal(databaseSpy);
+
+    const newData = await repository.createComic({
+      name: 'Dororo',
+      filePath: 'temp/dororo',
+    });
+
+    expect(newData).toEqual({
+      name: 'Dororo',
+      filePath: 'temp/dororo',
+      lastPageSeen: 1,
+    });
+  });
+
+  it('Throw error if create fail', async () => {
+    const databaseSpy = new ComicDatabaseSpy();
+    databaseSpy.error = new Error('Simulation Error');
+
+    const repository = new CreateComicRepositoryLocal(databaseSpy);
+
+    await expect(
+      repository.createComic({
+        name: 'Dororo',
+        filePath: 'temp/dororo',
+      }),
+    ).rejects.toEqual(Error('Simulation Error'));
+  });
 });
--- src/infra/repository/delete-comic-repository/delete-comic-repository.spec.ts
+++ src/infra/repository/delete-comic-repository/delete-comic-repository.spec.ts
@@ -1,3 +1,24 @@
+import ComicDatabaseSpy from '../../test/comic-database-spy';
+import DeleteComicRepositoryLocal from './delete-comic-repository-local';
+
 describe('Delete comic repository', () => {
-  it('', () => {});
+  it('Should return data if create successful', async () => {
+    const databaseSpy = new ComicDatabaseSpy();
+    const repository = new DeleteComicRepositoryLocal(databaseSpy);
+
+    await repository.deleteComic({id: '123'});
+
+    expect(databaseSpy.identifier).toEqual('123');
+  });
+
+  it('Throw error if create fail', async () => {
+    const databaseSpy = new ComicDatabaseSpy();
+    databaseSpy.error = new Error('Simulation Error');
+
+    const repository = new DeleteComicRepositoryLocal(databaseSpy);
+
+    await expect(repository.deleteComic({id: '123'})).rejects.toEqual(
+      Error('Simulation Error'),
+    );
+  });
 });
